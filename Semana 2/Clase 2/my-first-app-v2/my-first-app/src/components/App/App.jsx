17494c572dbbe97b92fa991e65524a72fbf9b1bc
Sr.Lechuga <jonattanlima@gmail.com>
2024-02-04 14:03:13 -0300

    Dianmic user table using props

--- Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/App/App.jsx
+++ Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/App/App.jsx
@@ -3,11 +3,38 @@ import Main from "../Main"
 import Footer from "../Footer";
 import "./App.css";
 
+const users = [
+  {
+    id: 1,
+    firstName: "Sr. Lechuga",
+    lastName: "Ensalada",
+    age: 29
+  },
+  {
+    id: 2,
+    firstName: "Sr. Tomate",
+    lastName: "Ensalada",
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: "Sr. Cebolla",
+    lastName: "Ensalada",
+    age: 36
+  },
+  {
+    id: 4,
+    firstName: "Sr. Maiz",
+    lastName: "Ensalada",
+    age: 17
+  },
+]
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main users={users}/>
       <Footer />
     </div>
   );
--- Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/Main/Main.jsx
+++ Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/Main/Main.jsx
@@ -1,9 +1,29 @@
-const Main = () => {
-    return (
-        <main>
-            <h1>Bienvenido Sr. Lechuga</h1>
-        </main>
-    );
+const Main = ({users}) => {
+  return (
+      <main>
+          <h1>Lista de usuarios</h1>
+          <table>
+              <thead>
+                  <tr>
+                      <th>Id</th>
+                      <th>Nombre</th>
+                      <th>Apellido</th>
+                      <th>Edad</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {users.map(({id,firstName,lastName,age}) => (
+                  <tr>
+                    <td>{id}</td>
+                    <td>{firstName}</td>
+                    <td>{lastName}</td>
+                    <td>{age}</td>
+                  </tr>
+                ))}
+              </tbody>
+          </table>
+      </main>
+  );
 };
 
 export default Main;
