5d5914a752aadc148f5edc1e67cacbb4df857be0
Sr.Lechuga <jonattanlima@gmail.com>
2024-02-04 13:39:01 -0300

    Changed to Main component

--- Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/App/App.jsx
+++ Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import Header from "../Header";
+import Main from "../Main"
 import Footer from "../Footer";
 import "./App.css";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <main>hola</main>
+      <Main />
       <Footer />
     </div>
   );
---
+++ Semana 2/Clase 2/my-first-app-v2/my-first-app/src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <main>
+            <h1>Bienvenido Sr. Lechuga</h1>
+        </main>
+    );
+};
+
+export default Main;
